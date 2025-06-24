71f1436ac84d1f85536473f19b4ff39956cc6873
nacretion <ananjev.nikita2017@ya.ru>
2025-06-24 12:20:40 +0500

    init typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import ormConfig from '../orm.config';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRoot(ormConfig),
+    ],
 })
 export class AppModule {}
